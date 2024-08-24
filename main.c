4a87fe4f8bf02a1ef632fb4cdfcb28eec02216db
= <w0ut3k3@gmail.com>
2024-08-24 19:42:32 +0200

    Added neighbour check method

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 #define LAB_WIDTH 4
 #define LAB_HEIGHT 4
 
+
 enum CellType { Empty = 0, Path = 1, Finish = 2 };
-typedef enum { NORTH, EAST, SOUTH, WEST} Direction;
+typedef enum Direction { NORTH, EAST, SOUTH, WEST} Direction;
 
 typedef struct {
 
@@ -14,8 +15,58 @@ typedef struct {
 
 } Player;
 
+Direction* scanForNeighbours(int playerX, int playerY, int labyrinth[LAB_HEIGHT][LAB_WIDTH]) {
 
-// TODO: Implement breadth first search.
+  Direction* neighbours = (Direction*)malloc(4 * sizeof(Direction));
+  printf("Player location: %d, %d\n", playerX, playerY);
+
+  printf("North = %d\n", labyrinth[playerY - 1][playerX]);
+  printf("East = %d\n", labyrinth[playerY][playerX + 1]);
+  printf("South = %d\n", labyrinth[playerY + 1][playerX]);
+  printf("West = %d\n", labyrinth[playerY][playerX - 1]);
+  
+  if (playerY != 0 && labyrinth[playerY - 1][playerX] == 1) {
+    neighbours[0] = NORTH;
+  }
+
+  if (playerX != LAB_WIDTH - 1 && labyrinth[playerY][playerX + 1] == 1) {
+    neighbours[1] = EAST;
+  }
+  
+  if (playerY != LAB_HEIGHT - 1 && labyrinth[playerY + 1][playerX] == 1) {
+    neighbours[2] = SOUTH;
+  }
+
+  if (playerX != 0 && labyrinth[playerY][playerX - 1] == 1) {
+    neighbours[3] = WEST;
+  }
+
+  return neighbours;
+  
+}
+
+int singlePathSearch(Player *player, int labyrinth[LAB_HEIGHT][LAB_WIDTH]){
+
+  int visitedMask[LAB_HEIGHT][LAB_WIDTH] = {0};
+
+  int *playerX = &player->location[0];
+  int *playerY = &player->location[1];
+
+  visitedMask[*playerY][*playerX] = 1;
+
+  Direction* neighbours = scanForNeighbours(*playerX, *playerY, labyrinth);
+
+  int size = 4;
+  printf("Amount of neighbours: %d\n\n", size);
+  for (int i = 0; i < size; i++) {
+    printf("Neighbours:");
+    printf("%d\n", (int)neighbours[i]);
+  }
+
+  free(neighbours);
+  return 0;
+}
+
 int BFS(Player *player, int labyrinth[LAB_HEIGHT][LAB_WIDTH]) { return 0; }
 
 int DFS(Player *player, int labyrinth[LAB_HEIGHT][LAB_WIDTH]) { return 0; }
@@ -29,15 +80,18 @@ int AStarSearch(Player *player, int labyrinth[LAB_HEIGHT][LAB_WIDTH]) { return 0
 int main() {
 
   int labyrinthMask[LAB_HEIGHT][LAB_WIDTH] = {{1, 1, 1, 1},
-			     {0, 0, 0, 1},
+			     {1, 0, 0, 1},
 			     {1, 1, 1, 1},
 			     {2, 0, 0, 0}};
 
 
   Player p1;
   p1.location[0] = 0;
-  p1.location[1] = 1;
+  p1.location[1] = 0;
   p1.direction = SOUTH;
+
+  singlePathSearch(&p1, labyrinthMask);
+
 
   return 0;
 }
